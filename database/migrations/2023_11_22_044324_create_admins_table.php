7972086ead94bcb54bd490d47085d44bdbe4bc14
Hafidzrdwn <hafidz10032005@gmail.com>
2023-11-22 13:31:04 +0700

    Add login_tokens column

--- database/migrations/2023_11_22_044324_create_admins_table.php
+++ database/migrations/2023_11_22_044324_create_admins_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('login_tokens')->nullable();
             $table->timestamps();
         });
     }
